c65949512bbf87c7f459cc32af7c5d40c5865519
Pea Lutz <pea@glocal.coop>
2016-02-03 00:05:44 -0500

    Registered new menu and added to header

--- functions.php
+++ functions.php
@@ -27,6 +27,55 @@
 
 $textdomain = "greenhorns";
 
+
+if ( ! function_exists( 'greenhorns_setup' ) ) :
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ *
+ * Create your own twentysixteen_setup() function to override in a child theme.
+ *
+ * @since Twenty Sixteen 1.0
+ */
+function greenhorns_setup() {
+    /*
+     * Make theme available for translation.
+     * Translations can be filed in the /languages/ directory.
+     * If you're building a theme based on Twenty Sixteen, use a find and replace
+     * to change 'twentysixteen' to the name of your theme in all the template files
+     */
+    load_theme_textdomain( $textdomain, get_template_directory() . '/languages' );
+
+    add_action( 'wp_enqueue_scripts', 'greenhorns_enqueue_styles' );
+
+
+    // This theme uses wp_nav_menu() in two locations.
+    register_nav_menus( array(
+        'external' => __( 'External Links Menu', $textdomain ),
+    ) );
+
+    $custom_header_defaults = array(
+        'width'                  => 1200,
+        'height'                 => 160,
+        'flex-height'            => true,
+        'flex-width'             => true,
+        'header-text'            => false,
+    );
+
+    add_theme_support( 'custom-header', $custom_header_defaults );
+
+}
+endif; // greenhorns_setup
+
+add_action( 'after_setup_theme', 'greenhorns_setup' );
+
+
+/**
+ * Enqueue parent and child styles
+ */
 function greenhorns_enqueue_styles() {
 
     $parent_style = 'parent-style';
@@ -37,5 +86,4 @@ function greenhorns_enqueue_styles() {
         array( $parent_style )
     );
 }
-add_action( 'wp_enqueue_scripts', 'greenhorns_enqueue_styles' );
 
