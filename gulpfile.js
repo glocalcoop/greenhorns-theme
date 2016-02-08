ba748e35c738f413e3b6092ef99b36db55d5c11b
Pea Lutz <pea@glocal.coop>
2016-02-08 18:16:15 -0500

    Changed social nav to use auto-generated sprites and Css.

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,8 @@ var gulp = require('gulp'),
 	rename = require( 'gulp-rename' ),
 	notify = require( 'gulp-notify' ),
 	include = require( 'gulp-include' ),
+    gulpif = require('gulp-if'),
+    sprity = require('sprity'),
 	sass = require( 'gulp-sass' );
 
 var onError = function( err ) {
@@ -42,15 +44,30 @@ gulp.task('scripts', function() {
     .pipe( livereload() );
 });
 
+// generate sprite.png and _sprite.scss 
+gulp.task('sprites', function () {
+  return sprity.src({
+    src: './src/images/sprites/**/*.{png,jpg}',
+    style: './_sprites.scss',
+    split: true,
+    cssPath: 'assets/images/',
+    // ... other optional options 
+    // for example if you want to generate scss instead of css 
+    processor: 'sass', // make sure you have installed sprity-sass 
+  })
+  .pipe(gulpif('*.png', gulp.dest('./assets/images/'), gulp.dest('./src/sass/components/')))
+});
+
 gulp.task( 'watch', function() {
 	livereload.listen();
 	gulp.watch( './src/sass/**/*.scss', [ 'sass' ] );
 	gulp.watch( './src/js/**/*.js', [ 'scripts' ] );
+    gulp.watch( './src/images/sprites/**/*.{png,jpg}', [ 'sprites' ] );
 	gulp.watch( './**/*.php' ).on( 'change', function( file ) {
 		livereload.changed( file );
 	} );
 } );
 
-gulp.task( 'default', [ 'sass', 'scripts', 'watch' ], function() {
+gulp.task( 'default', ['watch', 'sass', 'scripts', 'sprites' ], function() {
 
 } )
